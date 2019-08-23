06ee253f07b47d236d16e633f269eee68c167768
Megha.s <47857988+Nec0917014@users.noreply.github.com>
2019-08-23 15:49:33 +0530

    Add files via upload

---
+++ Addition.js
@@ -0,0 +1,22 @@
+var exports=module.exports={};
+exports.AddNumber=function(a,b)
+{
+return a+b;
+}
+exports.multiNumber=function(a,b)
+{
+return a*b;
+}  
+exports.subNumber=function(a,b)
+{
+return a-b;
+}
+exports.MultiNumber=function(a,b)
+{
+return a*a;
+}
+  I 
+exports.sqrtNumber=function(a)
+{
+return a*a;
+}
---
+++ function.js
@@ -0,0 +1,6 @@
+var Addition=require('./Addition.js');
+console.log(Addition.AddNumber(1,2));
+console.log(Addition.multiNumber(1,2));
+console.log(Addition.subNumber(1,2));
+console.log(Addition.MultiNumber(2));
+console.log(Addition.sqrtNumber(2));
